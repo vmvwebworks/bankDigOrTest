3a4b21bc5a025b92c92bc64dead44163c935d444
Marcos <vmvwebworks@gmail.com>
2018-06-30 10:30:46 +0200

    some tdd specs

--- Gemfile
+++ Gemfile
@@ -59,6 +59,7 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'database_cleaner'
 end
 group :development, :test do
   gem 'rspec-rails', '~> 3.7'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
-
+require 'database_cleaner'
+DatabaseCleaner.strategy = :truncation
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  puts "cleaning test db before running tests..."
+  DatabaseCleaner.clean
+  puts "seed db with banks..."
+  Rails.application.load_seed
+
+
+  it "is valid with valid attributes" do
+    user = User.create(name: "valid user", email: "validemail@specmarcos.co", password: "123456", bank_id: 1)
+    expect(user.name).to eq("valid user")
+    expect(user.bank.name).to eq("Bankinter")
+  end
 end
